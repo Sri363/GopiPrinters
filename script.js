326199aee4c60804747ccf234c5bd840db4b7c3f
Sri hatake <96491256+Sri363@users.noreply.github.com>
2023-08-02 12:50:30 +0530

    Product cards

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".hero-text-h1").addClass("animate__animated animate__fadeInUp")
+
+    $(".hero-text-h3").addClass("animate__animated animate__fadeInUp")
+});
