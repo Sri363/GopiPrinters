f673c1580e55cc430507d24759f128d174474658
Sri hatake <96491256+Sri363@users.noreply.github.com>
2023-08-02 17:33:08 +0530

    cards

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
-    $(".hero-text-h1").addClass("animate__animated animate__fadeInUp")
+    $(".hero-text-h1").addClass(
+        "animate__animated animate__fadeInUp"
+    );
 
-    $(".hero-text-h3").addClass("animate__animated animate__fadeInUp")
+    $(".hero-text-h3").addClass(
+        "animate__animated animate__fadeInUp"
+    );
+
+    $(".product-card").hover(function () {
+        $(this).find(".card-img-overlay").fadeIn();
+    },
+        function () {
+            $(this).find(".card-img-overlay").fadeOut();
+        }
+    )
 });
