266ded721cae4011c458b7bd668891985df412e9
Sri hatake <96491256+Sri363@users.noreply.github.com>
2023-08-10 16:29:22 +0530

    fadeupbig animation

--- script.js
+++ script.js
@@ -23,7 +23,34 @@ $(document).ready(function () {
         $(".about").addClass("animate__animated animate__fadeInRight")
     }, offset);
 
-    $(".product-card").waypoint(function () {
-        $(".product-card").addClass("animate__animated animate__fadeInUpBig")
+    $(".product-card:nth-of-type(1)").waypoint(function () {
+        $(".product-card:nth-of-type(1)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(2)").waypoint(function () {
+        $(".product-card:nth-of-type(2)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(3)").waypoint(function () {
+        $(".product-card:nth-of-type(3)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(4)").waypoint(function () {
+        $(".product-card:nth-of-type(4)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(5)").waypoint(function () {
+        $(".product-card:nth-of-type(5)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(6)").waypoint(function () {
+        $(".product-card:nth-of-type(6)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(7)").waypoint(function () {
+        $(".product-card:nth-of-type(7)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(8)").waypoint(function () {
+        $(".product-card:nth-of-type(8)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(9)").waypoint(function () {
+        $(".product-card:nth-of-type(9)").addClass("animate__animated animate__fadeInUpBig")
+    }, offset)
+    $(".product-card:nth-of-type(10)").waypoint(function () {
+        $(".product-card:nth-of-type(10)").addClass("animate__animated animate__fadeInUpBig")
     }, offset)
 });
